767dfa0487175cb42a6d8b3a8e01f9ed6f643382
AmitGit217 <amit217@yandex.com>
2026-02-05 21:13:03 +0100

    chore: ejercicio 24

---
+++ ej24.js
@@ -0,0 +1,10 @@
+const products = [
+  { name: "Funko Dr. Strange", sellCount: 10 },
+  { name: "Mochila de protones: Ghostbusters", sellCount: 302 },
+  { name: "Sable laser FX", sellCount: 23 },
+  { name: "Varita de Voldemort", sellCount: 6 },
+];
+
+const sum = products.reduce((accumulator, product) => accumulator + product.sellCount, 0);
+
+console.log(`El número total de ventas es: ${sum}`);
